68953f16a6156e621774cba960d5be268bf5f3ee
Paolo Salomone <paolo.salomone1212@gmail.com>
2021-10-24 23:02:28 +0200

    prova definitiva

--- lab_1a.cc
+++ lab_1a.cc
@@ -6,7 +6,7 @@
 using std::cout;
 using std::endl;
 
-// Constructors prova
+// Constructors 
 
 Complex::Complex() {
   x_=0;
---
+++ lab_1b.cpp
@@ -0,0 +1,11 @@
+// Lab_1b.cpp
+
+#include "lab_1b.h"
+
+int main() {
+
+  
+
+  return 0;
+}
+
