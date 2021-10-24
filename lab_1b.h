6b68cc1b930bc06030c372e34803d1c6a045cf69
PaoloSalomone <92625492+PaoloSalomone@users.noreply.github.com>
2021-10-24 22:52:11 +0200

    Semplice file.h non completo
    Volevo provare a crearlo da visual studio ma non me l ha fatto sincronizzare. Così l ho inserito a mano già che c ero

---
+++ lab_1b.h
@@ -0,0 +1,61 @@
+// Vector3D.h
+
+#include <iostream>
+using namespace std;
+
+class Vector3D {
+  public:
+    // constructors
+    Vector3D();
+    Vector3D(double x,double y,double z);
+    Vector3D(const Vector3D& Vector3D);
+
+
+    //setters
+    void setX (double x); 
+    void setY (double y); 
+    void setZ (double z);
+
+    //getters magnitude(), theta(), phi(),
+    double x();
+    double y();
+    double z(); 
+    double magnitude();
+    double theta();
+    double phi();
+
+
+    // Operators ??
+    Vector3D operator+ (const Vector3D& rhs) const;
+    Vector3D operator- (const Vector3D& rhs) const;
+    Vector3D operator* (const Vector3D& rhs) const;
+    Vector3D operator/ (const Vector3D& rhs) const;
+    Vector3D operator= (const Vector3D& rhs) const;
+    Vector3D operator+= (const Vector3D& rhs) const;
+    Vector3D operator-= (const Vector3D& rhs) const;
+    Vector3D operator*= (const Vector3D& rhs) const;
+    Vector3D operator/= (const Vector3D& rhs) const;
+  
+    // operators of assigment
+
+    //const Vector3D& operator+=(const Vector3D& rhs);
+    const Vector3D& operator-=(const Vector3D& rhs);
+    const Vector3D& operator*=(const Vector3D& rhs);
+    const Vector3D& operator/=(const Vector3D& rhs);
+
+    void operator+=(const Vector3D& rhs);
+
+    //moltiplication by a scalar
+
+    // moltiplication only of the form Vector3D*double
+    Vector3D operator* (const double& rhs) const;
+
+    //moltiplication of form double*Vector3D
+    friend Vector3D operator*(const double& lhs, const Vector3D& rhs);
+  
+    void reset();
+    void print();
+  private:
+    double x_,y_,z_;
+};
+  
