8e7f3e2517548a395f148e0644bf4ecb048fb3e8
DanieleScaffidi <92438528+DanieleScaffidi@users.noreply.github.com>
2021-11-15 11:24:59 +0100

    Add files via upload

--- 2/Datum.cc
+++ 2/Datum.cc
@@ -1,109 +1,20 @@
-#include "Datum.h"
 #include <iostream>
-#include <cmath>
-using std::cout;
-using std::endl;
-using std::ostream;
+#include "Datum.h"
 
-// Set static data member
-double Datum::tolerance_ = 1e-4;
+//constructor
+Datum::Datum() { value_ = 0; error_ = 0; }
+Datum::Datum(const double value, const double error) { value_ = value; error_ = error; }
+Datum::Datum(const Datum& datum) { value_ = datum.value_; error_ = datum.error_; }
+//destructor
+Datum::~Datum(){}
 
-//Constructors
-Datum::Datum() {
-  value_ = 0.0;
-  error_ = 0.0;
-}
+//getters
+double Datum::GetValue() const { return value_; }
+double Datum::GetError() const { return error_; }
 
-Datum::Datum(double x, double y) {
-  value_ = x;
-  error_ = y;
-}
+//setters
+void Datum::SetValue(double value) { value_ = value; }
+void Datum::SetError(double error) { error_ = error; }
 
-Datum::Datum(const Datum& datum) {
-  value_ = datum.value_;
-  error_ = datum.error_;
-}
-
-//Other methods
-double Datum::significance() const {
-  return value_/error_;
-}
-
-void Datum::print() const {
-  using namespace std;
-  cout << "datum: " << value_
-       << " +/- " << error_ << endl;
-}
-
-Datum Datum::sum(const Datum& rhs) const {
-  // sum of central values
-  double val = value_ + rhs.value_;
-  // assume data are uncorrelated: sum in quadrature of errors
-  double err = sqrt( error_*error_ + (rhs.error_)*(rhs.error_) );
-  // result
-  return Datum(val,err);
-}
-
-//Operator overloading
-Datum Datum::operator+(const Datum& rhs) const {
-  // sum of central values
-  double val = value_ + rhs.value_;
-  // assume data are uncorrelated: sum in quadrature of errors
-  double err = sqrt( error_*error_ + (rhs.error_)*(rhs.error_) );
-  // result
-  return Datum(val,err);
-  //avoid duplicate code
-  // return Datum::sum(rhs)
-}
-
-const Datum& Datum::operator+=(const Datum& rhs) {
-  value_ += rhs.value_;
-  error_ = sqrt( rhs.error_*rhs.error_ + error_*error_ );
-  return *this;
-}
-
-const Datum& Datum::operator=(const Datum& rhs) {
-  value_ = rhs.value_;
-  error_ = rhs.error_;
-  return *this;
-}
-
-bool Datum::operator==(const Datum& rhs) const {
-  return (fabs(value_-rhs.value_) < tolerance_ &&
-	  fabs(error_-rhs.error_) < tolerance_);
-}
-
-bool Datum::operator<(const Datum& rhs) const {
-  return ( value_ < rhs.value_ );
-}
-
-Datum Datum::operator*(const Datum& rhs) const {
-  double val = value_*rhs.value_;
-  // error propagation for x*y
-  double err = sqrt( rhs.value_*rhs.value_*error_*error_ +
-                     rhs.error_*rhs.error_*value_*value_ );
-  return Datum(val,err);
-}
-
-Datum Datum::operator/(const Datum& rhs) const {
-  double val = value_ / rhs.value_;
-  // error propagation for x/y
-  double err = fabs(val) * sqrt( (error_/value_)*(error_/value_) +
-                           (rhs.error_/rhs.value_)*(rhs.error_/rhs.value_) );
-  return Datum(val,err);
-}
-
-Datum Datum::operator*(const double& rhs) const {
-  return Datum(value_*rhs,error_*rhs);
-}
-
-// global functions
-Datum operator*(const double& lhs, const Datum& rhs){
-  return Datum(lhs*rhs.value_, lhs*rhs.error_);
-}
-
-ostream& operator<<(ostream& os, const Datum& rhs){
-  using namespace std;
-  os << rhs.value_ << " +/- " << rhs.error_; // NB: no endl!
-  return os;
-}
+//print
+void Datum::print() { std::cout << "Datum: value" << value_ << "+/-" << error_ << std::endl; }
--- 2/Datum.h
+++ 2/Datum.h
@@ -1,47 +1,33 @@
+#pragma once
 #ifndef Datum_h
 #define Datum_h
-#include <iostream>
 
-class Datum {
-  public:
-    //Constructors
-    Datum();
-    Datum(double x, double y);
-    Datum(const Datum& datum);
+class Datum
+{
+public:
 
-    //Destructor
-    ~Datum() { };
+	//constructor
+	Datum();
+	Datum(const double value, const double error);
+	Datum(const Datum& datum);
+	//destructor
+	~Datum();
 
-    //Getters
-    double value() const { return value_; }
-    double error() const { return error_; }
+	//getters
+	double GetValue() const;
+	double GetError() const;
 
-    //Setters
-    void setValue(double v) { value_ = v; }
-    void setError(double e) { error_ = e; }
-    static void setTolerance(double val) { tolerance_ = val; };
+	//setters
+	void SetValue(double value);
+	void SetError(double erorr);
 
-    //Other methods
-    double significance() const;
-    void print() const;
-    Datum sum( const Datum& rhs ) const;
+	//print
+	void print();
 
-    //Operator overloading
-    Datum operator+(const Datum& rhs) const;
-    const Datum& operator+=(const Datum& rhs);
-    const Datum& operator=(const Datum& rhs);
-    bool operator==(const Datum& rhs) const;
-    bool operator<(const Datum& rhs) const;
-    Datum operator*(const Datum& rhs) const;
-    Datum operator/(const Datum& rhs) const;
-    Datum operator*(const double& rhs) const;
-    //Will use global functions
-    friend Datum operator*(const double& lhs, const Datum& rhs);
-    friend std::ostream& operator<<(std::ostream& os, const Datum& rhs);
+private:
 
-  private:
-    double value_;
-    double error_;
-    static double tolerance_;
+	double value_;
+	double error_;
+
 };
-#endif
+#endif // !Datum
